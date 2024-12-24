64ed4c191d343e27fd8bb39ae1c1a3c24323aa95
umarkhanwah <umarkhanwah044@gmail.com>
2024-12-24 15:48:53 +0500

    choti si galti

--- cleint/src/App.jsx
+++ cleint/src/App.jsx
@@ -1,5 +1,5 @@
 
-import Home from './pages/home';
+import Home from './pages/Home';
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
