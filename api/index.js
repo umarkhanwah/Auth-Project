354080cd94c7d86ce37e2c8b5b1a09b1394f2401
umarkhanwah <umarkhanwah044@gmail.com>
2024-12-24 14:19:22 +0500

    route problem

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGO ).then(
 // Working for hosting
 app.use(express.static(path.join(__dirname , '/cleint/dist' , )))
 
-app.get("*" , (req , res)=>{
+app.get("/" , (req , res)=>{
     // res.sendFile(path.join(__dirname ,'cleint'  ,'dist' , 'index.html' ))
     res.send("Hellow")
 });
