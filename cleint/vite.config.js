50bc1373117ee29670175ed514bd3c34e53aa2c5
umarkhanwah <umarkhanwah044@gmail.com>
2024-12-24 15:39:24 +0500

    just commit

--- cleint/vite.config.js
+++ cleint/vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    proxy : {
-      '/api' : {
-        target  : "https://auth-project-umber.vercel.app/",
-        changeOrigin  : true
+  build: {
+    outDir: "dist", // Ensure the build output goes to the "dist" folder
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://auth-project-umber.vercel.app/',
+        changeOrigin: true,
+        secure: false,
       },
     },
-  }
-})
+  },
+});
